fca3c22a99a9099a97dca180b6e9e5a54cfe73ae
FurkanVural71 <furkanvural71@gmail.com>
2022-10-26 11:48:58 +0300

    oluşturuldu.

--- src/functions.js
+++ src/functions.js
@@ -1,103 +1,83 @@
-function addToCart(quantity,productName="Elma") {
-    console.log("Sepete eklendi : ürün : " 
-    + productName + " adet : " + quantity)
+function addToCart(productName="Elma",quantity) {
+    console.log("Sepete Eklendi : " + productName + " adet : " + quantity)
 }
 
-//addToCart()
-addToCart(10)
-//addToCart(15)
+addToCart("Elma")
+addToCart("Yumurta",10)
+addToCart("Yarak")
 
-let sayHello = ()=>{
-    console.log("Hello World!")
+let sayHello = () => {
+    console.log("Hello Amınoğlu!")
 }
 
 sayHello()
 
 let sayHello2 = function () {
-    console.log("Hello World 2")
+    console.log("Hello Amın Diğer Oğlu")
 }
 
-sayHello2();
+sayHello2()
 
-function addToCart2(productName, quantity, unitPrice) {
-    
+let product1 = {productName:"Yarak", unitPrice:31, quantity:69}
+
+function addToCart3(product){
+    console.log("Ürün : " +product.productName)
+    console.log("Adet : " +product.quantity)
+    console.log("Fiyat : " +product.unitPrice)
 }
-
-addToCart2("Elma",5,10)
-addToCart2("Armut", 2, 20)
-addToCart2("Limon",3,15)
-
-let product1 = {productName:"Elma", unitPrice:10, quantity:5}
-
-function addToCart3(product) {
-    console.log("Ürün : "+product.productName)
-    console.log("Adet : "+product.quantity)
-    console.log("Fiyat : "+product.unitPrice)
-}
-
 
 addToCart3(product1)
 
-let product2 = {productName:"Elma", unitPrice:10, quantity:5}
-let product3 = {productName:"Elma", unitPrice:10, quantity:5}
-product2 = product3
-product2.productName = "KARPUZ"
-console.log(product3.productName)
-
-let sayi1 = 10
-let sayi2 = 20
-sayi1 = sayi2
-sayi2 = 100
-console.log(sayi1)
-
-function addToCart4(products) {
+function addToCart4(x) {
     console.log(products)
+    
 }
 
 let products = [
-    {productName:"Elma", unitPrice:10, quantity:5},
-    {productName:"Armut", unitPrice:10, quantity:5},
-    {productName:"Karpuz", unitPrice:10, quantity:5}
+    {productName:"Yarak", unitPrice:31, quantity:69},
+    {productName:"Göt", unitPrice:31, quantity:69},
+    {productName:"Amcık", unitPrice:31, quantity:69}
 ]
 
 addToCart4(products)
 
-function add(bisey,...numbers) { //rest
-    let total = 0;
+function add(...numbers) { //rest
+    let total = 0
     for (let i = 0; i < numbers.length; i++) {
-        total = total + numbers[i]
+        total = total+numbers[i];
+        
     }
     console.log(total)
-    console.log(bisey)
+    
 }
 
-add(20,30)
-//add(20,30, 40)
-//add(20,30, 40, 50)
+add(31,69)
 
-let numbers = [30,10,500,600,120]
-//console.log(...numbers)
-console.log(Math.max(...numbers))
+let numbers = [31,69,17,471,61]
 
-let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
-    {name:"İç Anadolu", population:"20M"},
-    {name:"Marmara", population:"30M"},
-    {name:"Karadeniz", population:"10M"},
+console.log(...numbers)
+console.log(Math.max(...numbers))//spread
+
+//distracting
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri,marmaraSehirleri]] = [
+    {name:"İç Anadolu", population:"31M"},
+    {name:"Marmara", population:"40M"},
+    {name:"Karadeniz", population:"20M"},
     [
         ["Ankara","Konya"],
         ["İstanbul","Bursa"],
-        ["Sinop","Trabzon"],
+        ["Trabzon","Samsun"]
     ]
 ]
 
-// console.log(icAnadolu.name)
-// console.log(icAnadolu.population)
-console.log(icAnadoluSehirleri)
+console.log(icAnadolu)
+console.log(icAnadolu.name)
+console.log(icAnadolu.population)
+console.log(marmaraSehirleri)
 
 let newProductName, newUnitPrice, newQuantity
-({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
-= {productName:"Elma", unitPrice:10, quantity:5})
+({productName:newProductName,unitPrice:newUnitPrice,quantity:newQuantity}
+={productName:"Yarak", unitPrice:31, quantity:69})
 
 console.log(newProductName)
-console.log(newUnitPrice)
-console.log(newQuantity)
+
